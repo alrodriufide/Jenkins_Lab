0b29065a3abefebc270e717662dcddc21f4a709f
Allan <arodriguez70214@ufide.ac.cr>
2025-09-16 21:54:08 -0600

    test class modification

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,17 +1,11 @@
-package com.paymentchain.billing;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class BasicApplicationTests  {
+public class BasicApplicationTests {
 
     @Test
     public void contextLoads() {
-       String message = "Default Message cambio test devops";
-        Assert.assertNotNull(message);
+        String message = "Default Message cambio test devops";
+        assertNotNull(message);
     }
 }
